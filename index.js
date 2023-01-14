7d797b3b40de19e21d87fb27a6ae6ac1ba56c8ba
swethatheresa <swethatheresa123@gmail.com>
2023-01-14 17:40:08 +0530

    new commit

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const Gallery = require("./content");
 const app = express();
 const cors = require("cors");
 app.use(express.json());
-app.use(cors());
+require('dotenv').config();
+const port = process.env.PORT || 5000
+app.use(cors())
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 app.post("/add", async (req, res) => {
   let gallery = new Gallery(req.body);
@@ -18,5 +26,7 @@ app.get("/content", async (req, res) => {
   else res.send({ result: "error" });
 });
 
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
 
-app.listen(5000);
