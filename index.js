9a0d24f6af3149145ecd644cdb19207372e69b21
swethatheresa <swethatheresa123@gmail.com>
2023-01-14 19:56:48 +0530

    new commit

--- index.js
+++ index.js
@@ -3,44 +3,8 @@ require("./config");
 const Gallery = require("./content");
 const app = express();
 const cors = require("cors");
+app.use(express.json());
 app.use(cors());
-const bodyParser = require('body-parser');
-
-require('dotenv').config();
-const port = process.env.PORT || 5000
-app.use(cors())
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
-
-
-app.get('/', (request, response) => {
-  response.json(
-    {
-      "message": "Welcome to the Hostel Management System API",
-      "routes": [
-        {
-          "route": "/add",
-          "methods": [
-            "POST"
-          ]
-        },
-        {
-          "route": "/content",
-          "methods": [
-            "GET"
-           
-          ]
-        }
-      ]
-    }
-  )
-})
-
-
 
 app.post("/add", async (req, res) => {
   let gallery = new Gallery(req.body);
@@ -48,15 +12,12 @@ app.post("/add", async (req, res) => {
   result = await result.toObject();
   res.send(result);
 });
+
 app.get("/content", async (req, res) => {
   let gallery = await Gallery.find().sort({ _id: "desc"});
   if (gallery.length > 0) res.send(gallery);
   else res.send({ result: "error" });
 });
 
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
-
-module.exports=app;
+app.listen(5000);
+module.exports = app;
