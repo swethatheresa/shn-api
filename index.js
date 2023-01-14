6af84fecfb38d1f0323f4d36f9eb5a4a50ffb05f
swethatheresa <swethatheresa123@gmail.com>
2023-01-14 19:25:49 +0530

    new commit

--- index.js
+++ index.js
@@ -3,8 +3,44 @@ require("./config");
 const Gallery = require("./content");
 const app = express();
 const cors = require("cors");
-app.use(express.json());
 app.use(cors());
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+const port = process.env.PORT || 5000
+app.use(cors())
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+
+app.get('/', (request, response) => {
+  response.json(
+    {
+      "message": "Welcome to the Hostel Management System API",
+      "routes": [
+        {
+          "route": "/add",
+          "methods": [
+            "POST"
+          ]
+        },
+        {
+          "route": "/content",
+          "methods": [
+            "GET"
+           
+          ]
+        }
+      ]
+    }
+  )
+})
+
+
 
 app.post("/add", async (req, res) => {
   let gallery = new Gallery(req.body);
@@ -19,4 +55,8 @@ app.get("/content", async (req, res) => {
 });
 
 
-app.listen(5000);
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
+module.exports=app;
